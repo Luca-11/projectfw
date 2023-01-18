bf0a9b93edccd14d54db5d4a5c16b65b99e5d58b
Luca Guillemot <luca.guillemot@gmail.com>
2023-01-18 13:43:03 +0100

    1.01.02

--- script.js
+++ script.js
@@ -14,60 +14,73 @@ gsap.to("#anim-1", {
   },
 });
 
-gsap.from("#rose", {
-  x: -600,
-  // Ce qui change par rapport à avant
-  scrollTrigger: {
-    trigger: "#rose",
-    // afficher les marqueurs de ScrollTrigger
-    // utile pour débugger
-    // position des marqueurs
-    markers: false,
-    start: "top 30%",
-    end: "bottom 50%",
-    // synchroniser avec le scroll
-    scrub: 1,
+gsap.fromTo(
+  "#rose",
+  {
+    x: -600,
   },
-});
+  {
+    x: 0,
+    // Ce qui change par rapport à avant
+    scrollTrigger: {
+      trigger: "#rose",
+      // afficher les marqueurs de ScrollTrigger
+      // utile pour débugger
+      // position des marqueurs
+      markers: false,
+      start: "top 30%",
+      end: "bottom 50%",
+      // synchroniser avec le scroll
+      scrub: 1,
+    },
+  }
+);
 
-gsap.from("#text-anim", {
-  x: 500,
-  // Ce qui change par rapport à avant
-  scrollTrigger: {
-    trigger: "#text-anim",
-    // afficher les marqueurs de ScrollTrigger
-    // utile pour débugger
-    markers: false,
-    // position des marqueurs
-    start: "top 30%",
-    end: "bottom 50%",
-    // synchroniser avec le scroll
-    scrub: 1,
+gsap.fromTo(
+  "#text-anim",
+  {
+    x: 500,
   },
-});
+  {
+    x: 0,
+    scrollTrigger: {
+      trigger: "#text-anim",
 
-gsap.from("#best", {
-  y: -1500,
-  // Ce qui change par rapport à avant
-  scrollTrigger: {
-    trigger: "#anim-2",
-    // afficher les marqueurs de ScrollTrigger
-    // utile pour débugger
-    markers: true,
-    // position des marqueurs
-    start: "top 35%",
-    end: "bottom 45%",
-    // synchroniser avec le scroll
-    scrub: 1,
+      markers: false,
+
+      start: "top 30%",
+      end: "bottom 50%",
+
+      scrub: 1,
+    },
+  }
+);
+
+gsap.fromTo(
+  "#best",
+  {
+    y: -1500,
   },
-});
+  {
+    y: 0,
+    scrollTrigger: {
+      trigger: "#anim-2",
+      start: "top 35%",
+      end: "bottom 45%",
+      scrub: 1,
+    },
+  }
+);
 
 gsap.to("#wave-rose", {
+  backgroundImage: "url('vibz-assets/wave-blue.png')",
   scrollTrigger: {
     trigger: "#anim-3",
     start: "top 10%",
     end: "bottom 80%",
-    pin: true,
+    // pin: true,
+    markers: true,
+    scrub: 0.5,
   },
 });
 
