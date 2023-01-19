f0d336114678b285806e26a79ddafacab9119b78
Luca Guillemot <luca.guillemot@gmail.com>
2023-01-19 20:17:57 +0100

    1.01.04

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-gsap.to("#anim-1", {
+// --------------------Titre et extra--------------------
+
+gsap.to("#title-animation", {
+  delay: 1,
   x: -1200,
   // Ce qui change par rapport à avant
   scrollTrigger: {
-    trigger: "#anim-1",
+    trigger: "#animation-1",
     // afficher les marqueurs de ScrollTrigger
     // utile pour débugger
     markers: false,
@@ -13,6 +16,58 @@ gsap.to("#anim-1", {
     scrub: 4.5,
   },
 });
+
+gsap.to("#subtext", {
+  x: -1200,
+  delay: 2,
+  // Ce qui change par rapport à avant
+  scrollTrigger: {
+    trigger: "#animation-1",
+    // afficher les marqueurs de ScrollTrigger
+    // utile pour débugger
+    markers: false,
+    // position des marqueurs
+    start: "top 10%",
+    end: "bottom 100%",
+    // synchroniser avec le scroll
+    scrub: 4.5,
+  },
+});
+
+gsap.to("#try", {
+  x: -1200,
+  delay: 3,
+  // Ce qui change par rapport à avant
+  scrollTrigger: {
+    trigger: "#animation-1",
+    // afficher les marqueurs de ScrollTrigger
+    // utile pour débugger
+    markers: false,
+    // position des marqueurs
+    start: "top 10%",
+    end: "bottom 100%",
+    // synchroniser avec le scroll
+    scrub: 4.5,
+  },
+});
+
+// ------------------- Animation - Actu ---------------------
+
+gsap.fromTo(
+  "#news-animation",
+  {
+    opacity: 0,
+    y: 100,
+  },
+  {
+    y: 0,
+    duration: 1,
+    opacity: 1,
+    delay: 1,
+  }
+);
+
+// ----------------- Animation lateral -----------------
 
 gsap.fromTo(
   "#pink",
@@ -39,7 +94,7 @@ gsap.fromTo(
 gsap.fromTo(
   "#text-animation",
   {
-    x: 500,
+    x: 600,
   },
   {
     x: 0,
@@ -55,6 +110,8 @@ gsap.fromTo(
     },
   }
 );
+
+// ----------------- Animation best-text --------------------
 
 gsap.fromTo(
   "#best",
@@ -72,43 +129,163 @@ gsap.fromTo(
   }
 );
 
+// ----------- PIN wave-rose ----------------
+
 gsap.to("#wave-pink", {
   scrollTrigger: {
-    trigger: "#animation-3",
+    trigger: "#pin-animation",
     start: "top 10%",
-    end: "bottom 80%",
+    end: "bottom 20%",
     pin: true,
-    markers: true,
+    markers: false,
     scrub: 0.5,
   },
 });
 
-const timelineplane = gsap.timeline({
+// ------------------ Animation photos + chemin -------------------
+
+// ----------Chemin-------------
+
+gsap.to("#music", {
+  duration: 10,
+  scale: 0.5,
+  opacity: 0,
+  motionPath: {
+    path: "#motionPath path",
+    align: "#motionPath path",
+    autoRotate: true,
+    alignOrigin: [0.5, 0.5],
+  },
+  ease: "none",
   scrollTrigger: {
-    trigger: "#plane",
+    trigger: ".grid",
     markers: false,
-    // position des marqueurs
-    start: "top 10%",
-    end: "bottom 90%",
-    // synchroniser avec le scroll
-    scrub: 6,
+    start: "top 50%",
+    end: "bottom 10%",
+    scrub: 3,
   },
 });
-timelineplane.to(
-  "#plane",
-  {
-    duration: 15,
-    motionPath: {
-      path: "#motionPath path",
-      align: "#motionPath path",
-      autoRotate: true,
-      alignOrigin: [0.5, 0.5],
-    },
-    ease: "none",
-  },
-  0
-);
 
-// idée : h1 sort sur la gauche en 1er
-// ensuite le texte en-dessous puis le bouton en dernier avec un délai différent de chacun
-// photoshop la vague rose pour lui changer de couleur : faire img 1 --> que la rose / img2 --> zone blanche
+gsap.to(".grid", {
+  backgroundColor: "#64c3d4",
+  scrollTrigger: {
+    trigger: ".grid",
+    // afficher les marqueurs de ScrollTrigger
+    // utile pour débugger
+
+    // position des marqueurs
+    markers: false,
+    start: "top 30%",
+    end: "bottom 50%",
+    scrub: 3,
+  },
+});
+
+// ------------- Photos -----------------
+
+gsap.to(".photo-1", {
+  delay: 0.2,
+  autoAlpha: 1,
+  duration: 4,
+  scrollTrigger: {
+    markers: false,
+    trigger: "#music",
+    start: "top 10%",
+    end: "bottom 100%",
+  },
+});
+
+gsap.to(".photo-2", {
+  delay: 0.3,
+  autoAlpha: 1,
+  duration: 4,
+  scrollTrigger: {
+    markers: false,
+    trigger: "#music",
+    start: "top 10%",
+    end: "bottom 100%",
+  },
+});
+
+gsap.to(".photo-3", {
+  delay: 0.4,
+  autoAlpha: 1,
+  duration: 4,
+  scrollTrigger: {
+    markers: false,
+    trigger: "#music",
+    start: "top 10%",
+    end: "bottom 100%",
+  },
+});
+
+gsap.to(".photo-4", {
+  delay: 0.9,
+  autoAlpha: 1,
+  duration: 4,
+  scrollTrigger: {
+    markers: false,
+    trigger: "#music",
+    start: "top 10%",
+    end: "bottom 100%",
+  },
+});
+
+gsap.to(".photo-5", {
+  delay: 1,
+  autoAlpha: 1,
+  scrollTrigger: {
+    markers: false,
+    trigger: "#music",
+    start: "top 10%",
+    end: "bottom 100%",
+  },
+});
+
+gsap.to(".photo-6", {
+  delay: 1.1,
+  autoAlpha: 1,
+  duration: 4,
+  scrollTrigger: {
+    markers: false,
+    trigger: "#music",
+    start: "top 10%",
+    end: "bottom 100%",
+  },
+});
+
+gsap.to(".photo-7", {
+  delay: 1.6,
+  autoAlpha: 1,
+  duration: 4,
+  scrollTrigger: {
+    markers: false,
+    trigger: "#music",
+    start: "top 10%",
+    end: "bottom 100%",
+  },
+});
+
+gsap.to(".photo-8", {
+  delay: 1.7,
+  autoAlpha: 1,
+  duration: 4,
+  scrollTrigger: {
+    markers: false,
+    trigger: "#music",
+    start: "top 10%",
+    end: "bottom 100%",
+  },
+});
+
+gsap.to(".photo-9", {
+  delay: 1.8,
+  autoAlpha: 1,
+  duration: 4,
+  scrollTrigger: {
+    markers: false,
+    trigger: ".grid",
+    start: "top 10%",
+    end: "bottom 100%",
+  },
+});
